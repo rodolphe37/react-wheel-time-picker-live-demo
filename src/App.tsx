9900c3abd69c1e0516b7abc472ea1dcfb75bfaf7
rodolphe <rodolphe@MBP-de-rodolphe.lan>
2024-09-10 17:58:53 +0200

    some minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -31,101 +31,17 @@ function App() {
     <div
       className="App"
       style={{
-        width: "100%",
+        width: "100vw",
         height: "100vh",
-        paddingTop: 20,
+
         backgroundColor: isDarkMode ? "#1d1d1d" : "#f6f6f6",
       }}
     >
-      <button
-        style={{ backgroundColor: isDarkMode ? "#f6f6f6" : "#e2e2e2" }}
-        onClick={handleDarkModeSwitch}
-      >
-        Switch to {!isDarkMode ? "Dark" : "Light"} mode
-      </button>
-      <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
-        <div
-          style={{
-            flexDirection: "row",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexWrap: "wrap",
-          }}
-        >
-          <img src={reactLogo} className="logo react" alt="React logo" />
-          <h1 style={{ color: isDarkMode ? "#fff" : "gray" }}>
-            react-wheel-time-picker
-          </h1>
-        </div>
-      </div>
-      <h2 style={{ marginTop: 20, color: isDarkMode ? "#fff" : "" }}>
-        🔥 A wheel style typescript time picker for React. 🔥
-      </h2>
-      <p className="read-the-docs">
-        Written in typescript, with dark & light Mode, 12h or 24h, no moment.js
-        or date.fns needed, zero dependencies, easy to integrate & easy to
-        use...
-      </p>
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <h3 style={{ color: isDarkMode ? "#fff" : "gray" }}>Demo</h3>
-        <button
-          style={{ backgroundColor: isDarkMode ? "#f6f6f6" : "#e2e2e2" }}
-          onClick={hancleClick12Hours}
-        >
-          Switch to {!is12Hours ? "12H" : "24H"} format
-        </button>
-        <div
-          style={{
-            marginTop: 10,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: "100%",
-            height: 130,
-            border: "1px dotted gray",
-            boxShadow: "0px 1px 2px gray",
-            borderRadius: 8,
-            padding: 20,
-          }}
-        >
-          <div style={{ width: 150 }}>
-            <TimePicker
-              use12Hours={is12Hours ? true : false}
-              label="Start time"
-              isDarkMode={isDarkMode}
-              onChange={onChangeStartHours}
-              value={starthHourValue}
-            />
-          </div>
-          <div style={{ width: 150 }}>
-            <TimePicker
-              use12Hours={is12Hours ? true : false}
-              label="End time"
-              isDarkMode={isDarkMode}
-              onChange={onChangeEndHour}
-              value={endHourValue}
-            />
-          </div>
-        </div>
-      </div>
+          alignItems: "flex-start",
 
-      <div style={{ height: 80 }} />
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          position: "fixed",
-          bottom: 4,
         }}
       >
         <a
@@ -143,6 +59,95 @@ function App() {
           <img className="link_logo" src={NpmLogo} width={40} height={40} />
         </a>
       </div>
+      <button
+        style={{ backgroundColor: isDarkMode ? "#f6f6f6" : "#e2e2e2" }}
+        onClick={handleDarkModeSwitch}
+      >
+        Switch to {!isDarkMode ? "Dark" : "Light"} mode
+      </button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            flexDirection: "row",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          <img
+            src={reactLogo}
+            width={50}
+            className="logo react"
+            alt="React logo"
+          />
+          <h1 style={{ fontSize: 22, color: isDarkMode ? "#fff" : "gray" }}>
+            react-wheel-time-picker
+          </h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            flexWrap: "wrap",
+            width: "60%",
+            marginLeft: -40,
+          }}
+        >
+          <h3 style={{ color: isDarkMode ? "#fff" : "gray" }}>Demo</h3>
+          <button
+            style={{ backgroundColor: isDarkMode ? "#f6f6f6" : "#e2e2e2" }}
+            onClick={hancleClick12Hours}
+          >
+            Switch to {!is12Hours ? "12H" : "24H"} format
+          </button>
+          <div
+            style={{
+              marginTop: 10,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+              height: 130,
+              border: "1px dotted gray",
+              boxShadow: "0px 1px 2px gray",
+              borderRadius: 8,
+              padding: 20,
+            }}
+          >
+            <div style={{ width: 150 }}>
+              <TimePicker
+                use12Hours={is12Hours ? true : false}
+                label="Start time"
+                isDarkMode={isDarkMode}
+                onChange={onChangeStartHours}
+                value={starthHourValue}
+              />
+            </div>
+            <div style={{ width: 150 }}>
+              <TimePicker
+                use12Hours={is12Hours ? true : false}
+                label="End time"
+                isDarkMode={isDarkMode}
+                onChange={onChangeEndHour}
+                value={endHourValue}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ height: 80 }} />
     </div>
   );
 }
